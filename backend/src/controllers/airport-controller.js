b0f41743ce15a78a8e2f0fa610880729750338ad
Nisarg118 <nisargbhamat12@gmail.com>
2025-05-09 18:24:41 +0530

    added airport APIs

---
+++ backend/src/controllers/airport-controller.js
@@ -0,0 +1,101 @@
+const { StatusCodes } = require("http-status-codes");
+const { AirportService } = require("../services");
+const { ErrorResponse, SuccessResponse } = require("../utils/common");
+
+// POST: /airports
+// req-body: {name:"",code:"",address:"",cityId:""}
+
+async function createAirport(req, res) {
+  try {
+    const { name, code, address, cityId } = req.body;
+    const airport = await AirportService.createAirport({
+      name,
+      code,
+      address,
+      cityId,
+    });
+    SuccessResponse.data = airport;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
+  }
+}
+
+// GET: /airports
+// req-body: {}
+
+async function getAirports(req, res) {
+  try {
+    const airports = await AirportService.getAirports();
+    SuccessResponse.data = airports;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
+  }
+}
+
+// GET: /airports/:id
+// req-body: {}
+async function getAirport(req, res) {
+  try {
+    const airport = await AirportService.getAirport(req.params.id);
+    SuccessResponse.data = airport;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
+  }
+}
+
+// DELETE: /airports/:id
+// req-body: {}
+
+async function destroyAirport(req, res) {
+  try {
+    const airports = await AirportService.destroyAirport(req.params.id);
+    SuccessResponse.data = airports;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
+  }
+}
+
+// PATCH: /airports/:id
+// req-body: {}
+
+async function updateAirport(req, res) {
+  try {
+    const airports = await AirportService.updateAirport(
+      req.params.id,
+      req.body
+    );
+    SuccessResponse.data = airports;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    console.error("Update Airplane Error:", error);
+
+    return res
+      .status(error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR)
+      .json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  createAirport,
+  getAirports,
+  getAirport,
+  destroyAirport,
+  updateAirport,
+};
--- backend/src/repositories/index.js
+++ backend/src/repositories/index.js
@@ -1,4 +1,5 @@
 const CityRepository = (module.exports = {
   AirplaneRepository: require("./airplane-repository"),
   CityRepository: require("./city-repository"),
+  AirportRepository: require("./airport-repository"),
 });
